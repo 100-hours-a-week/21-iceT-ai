0908bd2adec7671a5030ad6c337e42a466b73841
junn0s <freefree9758@icloud.com>
2025-05-02 11:01:01 +0900

    Feat: 로깅 등록, 예외 핸들러 처리 등록 (#1)

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-from src.core.exception_handlers import (
-    validation_exception_handler,
-    value_error_handler,
-    generic_exception_handler,
-    timeout_exception_handler,
-)
+from src.core.logger import setup_logging
+from src.core.exception_handlers import add_exception_handlers
 from src.routers.v1.solution_router import router as solution_router
-from pydantic import ValidationError
-from starlette.exceptions import HTTPException as StarletteHTTPException
+
+setup_logging()
 
 app = FastAPI(
     title="코딩테스트 도우미 서비스",
@@ -15,12 +11,7 @@ app = FastAPI(
 )
 
 # 예외 핸들러 등록
-app.add_exception_handler(ValidationError, validation_exception_handler)
-app.add_exception_handler(ValueError, value_error_handler)
-app.add_exception_handler(StarletteHTTPException, generic_exception_handler)
-app.add_exception_handler(Exception, generic_exception_handler)
-app.add_exception_handler(TimeoutError, timeout_exception_handler)
-
+add_exception_handlers(app)
 
 # API v1 라우터 등록
 app.include_router(
@@ -28,7 +19,6 @@ app.include_router(
     prefix="/api/v1",
     tags=["해설지 생성 기능"],
 )
-
 
 # health check
 @app.get("/healthz")
