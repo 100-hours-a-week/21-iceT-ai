ddabc3bd72c082fa159280093acf02863232a575
junn0s <freefree9758@icloud.com>
2025-05-14 10:48:41 +0900

    Chore: 로컬 실행 가능한 코드 주석에 표시

--- src/crawler/boj_crawler.py
+++ src/crawler/boj_crawler.py
@@ -53,6 +53,7 @@ def login_with_cookies(driver):
     })
     driver.get("https://www.acmicpc.net/")
 
+
 # 백준 문제를 크롤링하는 함수
 def crawl_boj_problem_with_selenium(driver, problem_id):
     url = f"https://www.acmicpc.net/problem/{problem_id}"
@@ -95,3 +96,43 @@ def crawl_boj_problem_with_selenium(driver, problem_id):
             "input_example": "",
             "output_example": ""
         }
+    
+
+# 로컬에서 실행 가능한 코드
+
+# def create_driver():
+#     options = Options()
+#     options.add_argument("--headless=new")
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument(f"user-agent={random.choice(USER_AGENTS)}")
+
+#     service = Service("/Users/junsu/Downloads/chromedriver-mac-arm64/chromedriver")
+#     driver = webdriver.Chrome(service=service, options=options)
+#     return driver
+
+# def login_with_cookies(driver):
+#     driver.get("https://www.acmicpc.net/")
+#     driver.add_cookie({
+#         'name': 'OnlineJudge',
+#         'value': os.getenv("BOJ_COOKIE_ONLINEJUDGE"),
+#         'domain': '.acmicpc.net',
+#         'path': '/',
+#         'httpOnly': True,
+#         'secure': True
+#     })
+#     driver.add_cookie({
+#         'name': 'bojautologin',
+#         'value': os.getenv("BOJ_COOKIE_AUTOLOGIN"),
+#         'domain': '.acmicpc.net',
+#         'path': '/',
+#         'httpOnly': True,
+#         'secure': True
+#     })
+
+# USER_AGENTS = [
+#     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+#     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15",
+#     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0",
+#     "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1"
+# ]
