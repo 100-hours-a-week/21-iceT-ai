6bee2294b72f1d47c3f4c942febc7fe320ffdfbb
junn0s <freefree9758@icloud.com>
2025-05-19 16:10:49 +0900

    Refactor: 프롬프트 기능 향상

--- src/core/prompt_templates.py
+++ src/core/prompt_templates.py
@@ -3,15 +3,15 @@ from langchain_core.prompts import PromptTemplate
 # 백준 문제 정보를 기반으로 해설을 생성하는 프롬프트 템플릿
 SOLUTION_PROMPT = PromptTemplate(
     template="""
-    아래 백준 문제 정보와 관련 기술 정보를 바탕으로, 출력 순서에 따라 **JSON 형식**으로만 해설을 작성하세요.
+    아래 '백준 문제 정보'와 '참고 문서'를 바탕으로, 출력 순서에 따라 **JSON 형식**으로만 해설을 작성하세요.
 
-    출력 순서는 다음과 같습니다.
+    해설 출력 순서는 다음과 같습니다.
     1. problemNumber(문제 번호)
-    2. problem_check(문제 개요 및 알고리즘 설명)
-    3. problem_solving(단계별 구체적인 풀이 방법. 구체적일수록 좋음)
+    2. problem_check(문제 개요 및 알고리즘 설명 - 알고리즘 설명은 참고 문서 참조하여 설명)
+    3. problem_solving(단계별 구체적인 풀이 방법. 풀이에 사용된 라이브러리의 경우 참고 문서 참조하여 설명)
     4. solution_code(정답 코드를 python, c++, java로 각각 작성)
 
-    문제 정보 : 
+    백준 문제 정보 : 
     문제 번호   : {problem_number}
     제목       : {title}
     설명       : {description}
