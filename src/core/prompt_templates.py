5798a183d63e7b8bbd3e9cd8322a87a8ebb36429
junn0s <freefree9758@icloud.com>
2025-05-21 11:46:22 +0900

    chore: 템플릿 수정

--- src/core/prompt_templates.py
+++ src/core/prompt_templates.py
@@ -4,11 +4,12 @@ from langchain_core.prompts import PromptTemplate
 SOLUTION_PROMPT = PromptTemplate(
     template="""
     아래 '백준 문제 정보'와 '참고 문서'를 바탕으로, 출력 순서에 따라 **JSON 형식**으로만 해설을 작성하세요.
+    '참고 문서' 는 알고리즘 설명에만 사용됩니다.
 
     해설 출력 순서는 다음과 같습니다.
     1. problemNumber(문제 번호)
-    2. problem_check(문제 개요 및 알고리즘 설명 - 알고리즘 설명은 참고 문서 참조하여 설명)
-    3. problem_solving(단계별 구체적인 풀이 방법. 풀이에 사용된 라이브러리의 경우 참고 문서 참조하여 설명)
+    2. problem_check(문제 개요 및 알고리즘 설명 - 알고리즘 설명은 참고 문서에 해당 알고리즘이 있는 경우 참조하여 구체적으로 설명)
+    3. problem_solving(단계별 구체적인 풀이 방법)
     4. solution_code(정답 코드를 python, c++, java로 각각 작성)
 
     백준 문제 정보 : 
