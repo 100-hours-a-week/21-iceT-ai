f5913ceef5d93cdc68b7c5a4d79d4fa66eba6dea
junn0s <freefree9758@icloud.com>
2025-06-23 09:57:12 +0900

    chore: url 추가

--- src/config.py
+++ src/config.py
@@ -21,7 +21,10 @@ settings = Settings()
 
 # 백앤드 설정
 BACKEND_URL     = os.getenv("BACKEND_URL")
-BACKEND_TIMEOUT = float(os.getenv("BACKEND_TIMEOUT", "10.0"))
+BAEKJUN_BACKEND_URL = os.getenv("BAEKJUN_BACKEND_URL")
+GETPROBLEM_BACKEND_URL = os.getenv("GETPROBLEM_BACKEND_URL")
+RECOMMEND_BACKEND_URL = os.getenv("RECOMMEND_BACKEND_URL")
+BACKEND_TIMEOUT = float(os.getenv("BACKEND_TIMEOUT", "30.0"))
 
 # 서비스 설정
 SERVICE_API_KEY = os.getenv("SERVICE_API_KEY")
