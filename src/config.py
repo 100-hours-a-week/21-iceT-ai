c4c839f94a0d09823dd239efd924fbd51e691306
junn0s <freefree9758@icloud.com>
2025-06-30 13:52:43 +0900

    fix: max token 수 변경 - 오류 원인 해결

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     # model: str = "gemini-2.5-flash-preview-05-20"
     model: str = "gemini-2.5-flash"
     temperature: float = 0.3
-    max_tokens: int = 16384
+    max_tokens: int = 65000
 
 settings = Settings() 
 
